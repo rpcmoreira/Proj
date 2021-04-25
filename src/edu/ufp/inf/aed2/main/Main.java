7c7e2fc08dd57ffee1524990b8abcaa57e43d62e
rpcmoreira <rpcardoso2@outlook.com>
2021-04-25 16:27:14 +0100

    try 1

--- src/edu/ufp/inf/aed2/main/Main.java
+++ src/edu/ufp/inf/aed2/main/Main.java
@@ -5,5 +5,6 @@ import edu.princeton.cs.algs4.*;
 public class Main{
     public static void main(String[] args) {
         System.out.println("Ohh it is bish\n");
+        System.out.println("wow");
     }
 }
