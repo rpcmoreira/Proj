b570bf4b093c6754f1506e7106f433b602967534
rpcmoreira <rpcardoso2@outlook.com>
2021-04-25 16:18:49 +0100

    try 1

--- src/edu/ufp/inf/aed2/main/Main.java
+++ src/edu/ufp/inf/aed2/main/Main.java
@@ -3,5 +3,7 @@ package edu.ufp.inf.aed2.main;
 import edu.princeton.cs.algs4.*;
 
 public class Main{
-
+    public static void main(String[] args) {
+        System.out.println("It is working\n");
+    }
 }
