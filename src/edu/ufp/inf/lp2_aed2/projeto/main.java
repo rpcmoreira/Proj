667c5728454b5c245c3c8be464a51368572c98db
rpcardoso <rpcardoso2@outlook.com>
2021-05-03 20:05:08 +0100

    laptop to pc

--- src/edu/ufp/inf/lp2_aed2/projeto/main.java
+++ src/edu/ufp/inf/lp2_aed2/projeto/main.java
@@ -1,7 +1,11 @@
 package edu.ufp.inf.lp2_aed2.projeto;
 
+import edu.princeton.cs.algs4.*;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        BinarySearchST <String, Integer> users = new BinarySearchST<String, Integer>();
+        BinarySearchST <String, Integer> caches = new BinarySearchST<String, Integer>();
+        BinarySearchST <String, Integer> itens = new BinarySearchST<String, Integer>();
     }
 }
